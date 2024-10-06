203eec4ef85307a3a8c984332d4083fc4ac421ad
Your Name <xmil>
2024-10-06 11:07:49 +0700

    fsds

--- belajar2.py
+++ belajar2.py
@@ -1,26 +1,85 @@
-inputpertama = 10
-inputdewi = 5
+# import time
 
-print(inputpertama+inputdewi)
-data1 = ''
-print(type(data1))
-data1 = bool(data1)
-print (data1)
-print(type(data1))
+
+# print('hallo dunia')
 
 
 
-#float
-print(type(2.2))
 
-#integer
-print(type(2))
 
-#bolean
-print(type(True)) 
 
-#string 
-print(type('hello'))
+# Ini adalah komentar, tidak akan di eksekusi oleh komputer
 
-#bilangan kompleks
-print(type(complex(1,2.3)))
+# contoh variable dengan nama dan umur adalah variable, udin dan 17 adalah value dari variable
+nama = 'udin'
+umur = '17'
+
+# print(nama)
+# print(umur)
+
+
+# contoh type data
+
+# int (integer), bilangan bulat
+umur = 17
+
+# float, bilangan desimal
+berat = 50.63
+
+# str (string), kalimat atau text apapun yang di ampit oleh tanda kutip
+nama = 'udin'
+
+# bool (boolean), nilai benar atau salah dengan True adalah benar dan False adalah salah
+menikah = False
+
+
+
+
+# contoh pengkondisian (if-else)
+
+# if menikah:
+#     print('udin sudah menikah')
+# else:
+#     print('udin belum menikah')
+
+
+
+# contoh perulangan/loop (while)
+# langkah = 0
+# while langkah<=100:
+#     print('udin sudah berlari sebanyak '+str(langkah) + ' langkah')
+#     langkah+=1
+#     time.sleep(0.02)
+# print('udin kecapean dan berhenti berlari')
+
+
+# contoh perulangan/loop (for)
+
+# Daftar buah dalam keranjang
+keranjang_buah = ["apel", "jeruk", "pisang", "mangga", "anggur"]
+# Menggunakan perulangan for untuk menghitung buah dalam keranjang
+# for buah in keranjang_buah:
+#     print(f"Udin menemukan {buah} dalam keranjang.")
+
+# print("Selesai menghitung buah-buahan!")
+
+
+
+# contoh fungsi
+
+# Fungsi untuk menghitung total harga belanja
+def hitung_total(harga_barang, jumlah_barang):
+    total = harga_barang * jumlah_barang
+    return total
+
+# Menggunakan fungsi hitung_total dalam kehidupan sehari-hari
+# harga_apel = 5000
+# jumlah_apel = 3
+# total_apel = hitung_total(harga_apel, jumlah_apel)
+# print(f"Total harga apel: Rp{total_apel}")
+
+# harga_pisang = 2000
+# jumlah_pisang = 5
+# total_pisang = hitung_total(harga_pisang, jumlah_pisang)
+# print(f"Total harga pisang: Rp{total_pisang}")
+
